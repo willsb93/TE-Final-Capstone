d02098d588ca82839d60bca57b489a79b8c736b7
WilliamRabuske <williamrabuske93@gmail.com>
2020-04-06 18:32:55 -0400

    finished help/topic DAO and adjusted chatbot.sql

--- vue/backend/database/chatbotDBSQL.sql
+++ vue/backend/database/chatbotDBSQL.sql
@@ -1,18 +1,25 @@
+BEGIN TRANSACTION;
 
-
+DROP TABLE IF EXISTS help_topics;
+DROP TABLE IF EXISTS curriculum;
 
 CREATE TABLE help_topics (
-id SERIAL,
-topic_name VARCHAR(150)
+id SERIAL PRIMARY KEY NOT NULL,
+topic_name VARCHAR(150) NOT NULL
 );
 
-BEGIN TRANSACTION;
+CREATE TABLE curriculum (
+id SERIAL PRIMARY KEY NOT NULL,
+topic VARCHAR(150)
+
+);
+
 
 INSERT INTO help_topics (topic_name) 
 VALUES('Curriculum'),
-        ('Pathways Program'),
-        ('Motivation'),
-        ('Finding Open Positions');
+      ('Pathways Program'),
+      ('Motivation'),
+      ('Finding Open Positions');
 
 /*
 DELETE FROM help_topics;
@@ -20,14 +27,10 @@ DELETE FROM help_topics;
 
 SELECT * FROM help_topics;        
         
-ROLLBACK;
+COMMIT;
         
     
-CREATE TABLE curriculum (
-id SERIAL,
-topic VARCHAR(150)
 
-);
 
 
 
--- vue/backend/src/main/java/com/techelevator/model/JdbcTopicDao.java
+++ vue/backend/src/main/java/com/techelevator/model/JdbcTopicDao.java
@@ -3,37 +3,62 @@ package com.techelevator.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 
+
 @Component
-
 public class JdbcTopicDao implements TopicDao {
-	private jdbcTemplate; 
 
-	public JdbcTopicDao() {
-		
-		
-		
-		
-		// TODO Auto-generated constructor stub
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JdbcTopicDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
+	
 	@Override
 	public List<Topic> getAllTopics() {
-		List<Topic> allhelpTopics = new ArrayList<Topic>();	
-		
+		List<Topic> allHelpTopics = new ArrayList<Topic>();
+
 		String sqlGetAllTopics = "SELECT id, topic_name FROM help_topics";
-	 	
-		
-		
-		return null;
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlGetAllTopics);
+
+		while (results.next()) {
+			Topic topic = null;
+			topic = mapRowToTopic(results);
+			allHelpTopics.add(topic);
+
+		}
+		return allHelpTopics;
+
 	}
 
 	@Override
 	public Topic getTopicByTopicName(String topicName) {
-		// TODO Auto-generated method stub
-		return null;
+		Topic specificTopic = null;
+		String getTopicByTopicName = "SELECT topic_name FROM help_topics WHERE topic_name = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(getTopicByTopicName, topicName);
+
+		if (results.next()) {
+			specificTopic = mapRowToTopic(results);
+		}
+
+		return specificTopic;
 	}
 
+	private Topic mapRowToTopic(SqlRowSet results) {
+		Topic topic = new Topic();
+		topic.setTopicName(results.getString("topicName"));
+		topic.setTopicId(results.getInt("topicID"));
+
+		return topic;
+
+	}
 }
