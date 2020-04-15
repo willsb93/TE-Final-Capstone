8f93f193568696d7d734dc25f344d3a83646c8e2
Ryan Brooks <ryan_c_brooks@yahoo.com>
2020-04-15 14:34:48 -0400

    home tweaks with increment text no change

--- vue/frontend/src/views/Home.vue
+++ vue/frontend/src/views/Home.vue
@@ -12,7 +12,7 @@
           <div class="container">
             <div class="columns is-vcentered">
               <div class="column is-6">
-                <h1 class="title is-spaced" style="color:#3273dc;">Increment++</h1>
+                <h1 class="title is-spaced" style="color:#3273dc">Increment++</h1>  <!--<img id="company-logo-img-small" src ="@/assets/newprofile.png" /> -->
                 <p class="subtitle is-3">A Chatbot for Education</p>
                 <p class="subtitle">We believe that academic support is critical for your students' success, so we created a next-gen chatbot that drives student success by providing students the resources they need on demand.</p>
               
@@ -253,6 +253,11 @@ html {
 
   } */
 
+
+#company-logo-img-small {
+  height: 1em;
+  width: auto;
+}
 
 </style>
 
