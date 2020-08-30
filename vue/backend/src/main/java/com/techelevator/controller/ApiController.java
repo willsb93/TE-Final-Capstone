77f31de6b7d2abcbb0d5c93f4199b14e657a2425
willsb93 <williamrabuske93@gmail.com>
2020-08-30 11:54:13 -0400

    Update ApiController.java

--- vue/backend/src/main/java/com/techelevator/controller/ApiController.java
+++ vue/backend/src/main/java/com/techelevator/controller/ApiController.java
@@ -65,22 +65,4 @@ public class ApiController {
     	
 		return motivationDao.getAllMotivation();
 	}
-
-    
-    
-    // Do we need this??
-//    @RequestMapping(path = "/", method = RequestMethod.GET)
-//    public String authorizedOnly() throws UnauthorizedException {
-//        /*
-//        You can lock down which roles are allowed by checking
-//        if the current user has a role.
-//        
-//        In this example, if the user does not have the admin role
-//        we send back an unauthorized error.
-//        */
-//        if (!authProvider.userHasRole(new String[] { "admin" })) {
-//            throw new UnauthorizedException();
-//        }
-//        return "Success";
-//    }
 }
